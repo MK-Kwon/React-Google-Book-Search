c3c6cebf6f87eb2bd411daa0bb4b724d8cbc7977
Min Kyung Kwon <tolitoli_78@hotmail.com>
2020-11-29 22:42:25 +1030

    added Saved component in Page folder

---
+++ client/src/pages/Saved.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Logo from '../components/Logo';
+import Bookshelf from '../components/Bookshelf';
+
+function Saved(){
+    return (
+        <>
+            <Navbar />
+            <Logo />
+            <Bookshelf />
+        </>
+    );
+}
+
+export default Saved;
