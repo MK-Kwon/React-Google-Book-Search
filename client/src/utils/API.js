76ce9cee31dbf5d966250070ba36143719868c33
Min Kyung Kwon <tolitoli_78@hotmail.com>
2020-11-29 22:44:15 +1030

    added BooksAPI with axios

---
+++ client/src/utils/API.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const BooksAPI = {
+    
+    searchBooks: function(query, API_KEY){
+        return axios.get('https://www.googleapis.com/books/v1/volumes?q=' + query + `&key=${API_KEY}`);
+    },
+    getBooks: function(){
+        return axios.get('/api/books');
+    },
+    getBook: function(id){
+        return axios.get('/api/books/' + id);
+    },
+    saveBook: function(book){
+        return axios.post('/api/books', {data: book});
+    },
+    removeBook: function(id){
+        return axios.delete('/api/books/' + id);
+    },
+    getAPIKEY: function(){
+        return axios.get('/api/apiKey');
+    }
+}; 
+
+export default BooksAPI;
